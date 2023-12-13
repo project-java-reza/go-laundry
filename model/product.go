a1099fba675b6cc3cd741fbc0b68c2c6e2ad8286
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-13 13:53:02 +0700

    [FEATURE] Struct Product based on field requirement

---
+++ model/product.go
@@ -0,0 +1,15 @@
+package model
+
+//type Product struct {
+//	Id    string
+//	Name  string
+//	Price int
+//	UomId string
+//}
+
+type Product struct {
+	Id    string
+	Name  string
+	Price string
+	Uom   Uom
+}
