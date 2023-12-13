7b80d3c8b2460153a0a2ef1c3454a64e89a01a69
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-13 11:29:21 +0700

    FIX

--- config/database.go
+++ config/database.go
@@ -6,19 +6,15 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// menggunakan huruf besar karena digunakan diluar ketika dipanggil
-// berkomunikasi antar layer menggunakan interface
 type DbConnection interface {
 	Conn() *sql.DB
 }
 
-// menggunakan huruf kecil karena hanya digunakan di file ini saja
 type dbConnection struct {
-	db  *sql.DB // data kosong
+	db  *sql.DB
 	cfg *Config
 }
 
-// pada saat d dipanggil maka otomatis mendapatkan db dan cfg yang ada di dbConnection
 func (d *dbConnection) initDb() error {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		d.cfg.DbConfig.Host,
@@ -32,30 +28,21 @@ func (d *dbConnection) initDb() error {
 	if err != nil {
 		return err
 	}
-	d.db = db // reassign dari data kosong db return pointer sql.DB setelah di open
+	d.db = db
 	return nil
 }
 
-// function receiver nya dbConnection dan returnnya pointer sql.DB
 func (d *dbConnection) Conn() *sql.DB {
 	return d.db
 }
 
-// Constructor
-// returnnya DbConnection dari interface, dan interface DbConnection cuma punya 1 method yang bisa diimplementasi yaitu Conn dan returnnya *sql.DB
 func NewDbConnection(cfg *Config) (DbConnection, error) {
-	// instance dbConnenction punya 2 field tapi disini hanya mengisi 1 field yaitu cfg
-	// kenapa cuma 1 field ? karena db digunakan pada saat initDb otomatis dbConnection akan keisi pada saat pemanggil struct
 	conn := &dbConnection{
 		cfg: cfg,
 	}
-	// conn sekarang sudah punya db
 	err := conn.initDb()
 	if err != nil {
 		return nil, err
 	}
 	return conn, nil
 }
-
-// clean architecture kalau butuh sebuah file hanya tinggal panggil Constructor dan codingan utamannya sudah ada di Constuctor
-// sekarang kalau butuh DbConnection tinggal panggil Constructor
