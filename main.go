de90b60a36d4c55d0225225b4679366d924a7ecd
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-14 12:28:25 +0700

    [IMPROVE] Implement CRUD Product

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"laundry/config"
-	"laundry/model"
 	"laundry/repository"
 )
 
@@ -28,7 +27,8 @@ func main() {
 
 	db := con.Conn()
 
-	uomRepo := repository.NewUomRepository(db)
+	// uomRepo := repository.NewUomRepository(db)
+	productRepo := repository.NewProductRepository(db)
 
 	// Create
 	//uomRepo.Save(model.Uom{
@@ -56,26 +56,26 @@ func main() {
 	//}
 
 	// UPDATE CARA 2
-	err = uomRepo.Update(model.Uom{
-		Id:          "2",
-		Description: "new_gr",
-	})
-
-	if err != nil {
-		fmt.Println("Error updating UOM: ", err)
-		return
-	}
-	fmt.Println("UOM updated successfully.")
+	//err = uomRepo.Update(model.Uom{
+	//	Id:          "2",
+	//	Description: "new_gr",
+	//})
+	//
+	//if err != nil {
+	//	fmt.Println("Error updating UOM: ", err)
+	//	return
+	//}
+	//fmt.Println("UOM updated successfully.")
 
 	// Get All
-	uoms, err := uomRepo.FindAll()
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		for _, uom := range uoms {
-			fmt.Printf("ID: %s, Name: %s\n", uom.Id, uom.Description)
-		}
-	}
+	//uoms, err := uomRepo.FindAll()
+	//if err != nil {
+	//	fmt.Println(err)
+	//} else {
+	//	for _, uom := range uoms {
+	//		fmt.Printf("ID: %s, Name: %s\n", uom.Id, uom.Description)
+	//	}
+	//}
 
 	// Delete By Id
 	//err = uomRepo.DeleteById("1")
@@ -84,5 +84,68 @@ func main() {
 	//} else {
 	//	fmt.Println("Delete Successfully!")
 	//}
+
+	// Create Product
+	//productRepo.Save(model.Product{
+	//	Id:    "1",
+	//	Name:  "Cuci",
+	//	Price: 10000,
+	//	Uom: model.Uom{
+	//		Id: "1",
+	//	},
+	//})
+
+	// Get By Id
+	//product, err := productRepo.FindById("1")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(product)
+
+	// Get Product By Name
+	//products, err := productRepo.FindByName("Cuci")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(products)
+
+	// GetAll Product
+	//products, err := productRepo.FindAll()
+	//for _, product := range products {
+	//	fmt.Printf("id: %s, Name: %s, Price: %s, Satuan: %s",
+	//		product.Id,
+	//		product.Name,
+	//		product.Price,
+	//		product.Uom.Description,
+	//	)
+	//}
+
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+
+	// Update Product
+	//err = productRepo.Update(model.Product{
+	//	Id:    "1",
+	//	Name:  "New Cuci",
+	//	Price: 15000,
+	//	Uom: model.Uom{
+	//		Id: "2",
+	//	},
+	//})
+	//
+	//if err != nil {
+	//	fmt.Println("Error updating Product: ", err)
+	//	return
+	//}
+	//fmt.Println("Product updated successfully.")
+
+	// Delete Product By Id
+	err = productRepo.DeleteById("1")
+	if err != nil {
+		fmt.Println("Error Deleting:", err)
+	} else {
+		fmt.Println("Delete Successfully!")
+	}
 
 }
--- model/product.go
+++ model/product.go
@@ -10,6 +10,10 @@ package model
 type Product struct {
 	Id    string
 	Name  string
-	Price string
-	Uom   Uom
+	Price int
+	Uom   Uom // supaya dapat uom beserta isi yang lain tidak hanya id
 }
+
+// Uom di Emmbeddable dari struct untuk mempermudah ketika select
+// SELECT p.id, p.name, p.price, u.id, u.name FROM product p
+// JOIN uom u ON p.uom_id = u.id
