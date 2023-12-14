df9ffff88d76d76d5b6a7845c7d571563dcc0c51
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-14 14:31:58 +0700

    [FEATURE] USECASE for design architecture

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"laundry/config"
+	"laundry/model"
 	"laundry/repository"
+	"laundry/usecase"
 )
 
 // Struct
@@ -27,125 +29,11 @@ func main() {
 
 	db := con.Conn()
 
-	// uomRepo := repository.NewUomRepository(db)
-	productRepo := repository.NewProductRepository(db)
-
-	// Create
-	//uomRepo.Save(model.Uom{
-	//	Id:          "2",
-	//	Description: "gr",
-	//})
-
-	// Get By Id
-	//uom, err := uomRepo.FindById("1")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println(uom)
-
-	// UPDATE CARA 1
-	//uomUpdate, err := uomRepo.FindById("1")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//uomUpdate.Description = "Gr"
-	//
-	//if err = uomRepo.Update(uomUpdate); err != nil {
-	//	fmt.Println(err.Error())
-	//	return
-	//}
-
-	// UPDATE CARA 2
-	//err = uomRepo.Update(model.Uom{
-	//	Id:          "2",
-	//	Description: "new_gr",
-	//})
-	//
-	//if err != nil {
-	//	fmt.Println("Error updating UOM: ", err)
-	//	return
-	//}
-	//fmt.Println("UOM updated successfully.")
-
-	// Get All
-	//uoms, err := uomRepo.FindAll()
-	//if err != nil {
-	//	fmt.Println(err)
-	//} else {
-	//	for _, uom := range uoms {
-	//		fmt.Printf("ID: %s, Name: %s\n", uom.Id, uom.Description)
-	//	}
-	//}
-
-	// Delete By Id
-	//err = uomRepo.DeleteById("1")
-	//if err != nil {
-	//	fmt.Println("Error Deleting:", err)
-	//} else {
-	//	fmt.Println("Delete Successfully!")
-	//}
-
-	// Create Product
-	//productRepo.Save(model.Product{
-	//	Id:    "1",
-	//	Name:  "Cuci",
-	//	Price: 10000,
-	//	Uom: model.Uom{
-	//		Id: "1",
-	//	},
-	//})
-
-	// Get By Id
-	//product, err := productRepo.FindById("1")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println(product)
-
-	// Get Product By Name
-	//products, err := productRepo.FindByName("Cuci")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println(products)
-
-	// GetAll Product
-	//products, err := productRepo.FindAll()
-	//for _, product := range products {
-	//	fmt.Printf("id: %s, Name: %s, Price: %s, Satuan: %s",
-	//		product.Id,
-	//		product.Name,
-	//		product.Price,
-	//		product.Uom.Description,
-	//	)
-	//}
-
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-
-	// Update Product
-	//err = productRepo.Update(model.Product{
-	//	Id:    "1",
-	//	Name:  "New Cuci",
-	//	Price: 15000,
-	//	Uom: model.Uom{
-	//		Id: "2",
-	//	},
-	//})
-	//
-	//if err != nil {
-	//	fmt.Println("Error updating Product: ", err)
-	//	return
-	//}
-	//fmt.Println("Product updated successfully.")
-
-	// Delete Product By Id
-	err = productRepo.DeleteById("1")
-	if err != nil {
-		fmt.Println("Error Deleting:", err)
-	} else {
-		fmt.Println("Delete Successfully!")
-	}
+	uomRepo := repository.NewUomRepository(db)
+	uomUC := usecase.NewUomUseCase(uomRepo)
+	uomUC.CreateNew(model.Uom{
+		Id:          "3",
+		Description: "Buah",
+	})
 
 }
