9613a048339a02cb60f7581a831373094c6d4a3f
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-14 09:21:20 +0700

    [IMPROVE] Implement CRUD

--- main.go
+++ main.go
@@ -29,14 +29,60 @@ func main() {
 	db := con.Conn()
 
 	uomRepo := repository.NewUomRepository(db)
-	uomRepo.Save(model.Uom{
-		Id:          "1",
-		Description: "Kg",
+
+	// Create
+	//uomRepo.Save(model.Uom{
+	//	Id:          "2",
+	//	Description: "gr",
+	//})
+
+	// Get By Id
+	//uom, err := uomRepo.FindById("1")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(uom)
+
+	// UPDATE CARA 1
+	//uomUpdate, err := uomRepo.FindById("1")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//uomUpdate.Description = "Gr"
+	//
+	//if err = uomRepo.Update(uomUpdate); err != nil {
+	//	fmt.Println(err.Error())
+	//	return
+	//}
+
+	// UPDATE CARA 2
+	err = uomRepo.Update(model.Uom{
+		Id:          "2",
+		Description: "new_gr",
 	})
 
-	uom, err := uomRepo.FindById("1")
+	if err != nil {
+		fmt.Println("Error updating UOM: ", err)
+		return
+	}
+	fmt.Println("UOM updated successfully.")
+
+	// Get All
+	uoms, err := uomRepo.FindAll()
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		for _, uom := range uoms {
+			fmt.Printf("ID: %s, Name: %s\n", uom.Id, uom.Description)
+		}
 	}
-	fmt.Println(uom)
+
+	// Delete By Id
+	//err = uomRepo.DeleteById("1")
+	//if err != nil {
+	//	fmt.Println("Error Deleting:", err)
+	//} else {
+	//	fmt.Println("Delete Successfully!")
+	//}
+
 }
--- repository/uom_repository.go
+++ repository/uom_repository.go
@@ -11,7 +11,7 @@ type UomRepository interface {
 	FindById(id string) (model.Uom, error) // SELECT By Id
 	FindAll() ([]model.Uom, error)         // SELECT * FROM m_uom
 	Update(uom model.Uom) error            // Update
-	DeleteById(id string)                  // Delete FROM m_uom where id = ?
+	DeleteById(id string) error            // Delete FROM m_uom where id = ?
 }
 
 // tidak dipakai di file lain
@@ -38,18 +38,52 @@ func (u uomRepository) FindById(id string) (model.Uom, error) {
 }
 
 func (u uomRepository) FindAll() ([]model.Uom, error) {
-	//TODO implement me
-	panic("implement me")
+	rows, err := u.db.Query("SELECT * FROM m_uom")
+	if err != nil {
+		return nil, err
+	}
+	var uoms []model.Uom
+	for rows.Next() {
+		var uom model.Uom
+		err := rows.Scan(&uom.Id, &uom.Description)
+		if err != nil {
+			return nil, err
+		}
+		uoms = append(uoms, uom)
+	}
+
+	// pesan error jika kegagalan dalam pengambilan query
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+	return uoms, nil
 }
 
 func (u uomRepository) Update(uom model.Uom) error {
-	//TODO implement me
-	panic("implement me")
+	_, err := u.db.Exec("UPDATE m_uom SET description= $1 where id= $2", uom.Description, uom.Id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (u uomRepository) DeleteById(id string) {
-	//TODO implement me
-	panic("implement me")
+//func (u uomRepository) Update(uom model.Uom) error {
+//	query := "UPDATE m_uom set description = $1 where id=$2"
+//
+//	_, err := u.db.Exec(query, uom.Description, uom.Id)
+//	if err != nil {
+//		return err
+//	}
+//	return nil
+//}
+
+func (u uomRepository) DeleteById(id string) error {
+	_, err := u.db.Exec("DELETE FROM m_uom WHERE id=$1", id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func NewUomRepository(db *sql.DB) UomRepository {
